4152c7d81037f8f6b7127a7b78da116c32397afc
Daniel Fang <danfang@uw.edu>
2015-04-16 06:33:48 +0000

    Adding bootstrapping file

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y dist0upgrade
+apt-get -y upgrade
+apt-get install postgresql-9.3 python-pip python-dev tmux htop unzip postgresql-contrib libpq-dev
+pip install psycopg2 sqlalchemy virtualenv requests joe
