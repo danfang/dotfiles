b193c85591981b4df7932b155a058efa39bc7117
Daniel Fang <danfang@uw.edu>
2015-04-18 06:47:53 +0000

    adding jdk to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get -y dist0upgrade
 apt-get -y upgrade
-apt-get install postgresql-9.3 python-pip python-dev tmux htop unzip postgresql-contrib libpq-dev
+apt-get install default-jdk postgresql-9.3 python-pip python-dev tmux htop unzip postgresql-contrib libpq-dev
 pip install psycopg2 sqlalchemy virtualenv requests joe
