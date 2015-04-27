1316777821ffb8130ce27d227ddd63e2e3b3c1a5
Daniel Fang <danfang@uw.edu>
2015-04-27 02:50:48 +0000

    Typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y dist0upgrade
+apt-get -y dist-upgrade
 apt-get -y upgrade
 apt-get install default-jdk postgresql-9.3 python-pip python-dev tmux htop unzip postgresql-contrib libpq-dev
 pip install psycopg2 sqlalchemy virtualenv requests joe
