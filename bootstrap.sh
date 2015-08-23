2145583276e48982ac54c0283a9786af1c616942
Daniel Fang <danfang@uw.edu>
2015-08-22 21:44:27 -0700

    Updating postgres version

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get -y dist-upgrade
 apt-get -y upgrade
-apt-get install default-jdk postgresql-9.3 python-pip python-dev tmux htop unzip postgresql-contrib libpq-dev
+apt-get install default-jdk postgresql-9.4 postgresql-client-9.4 postgresql-contrib-9.4 python-pip python-dev tmux htop unzip libpq-dev
 pip install psycopg2 sqlalchemy virtualenv requests joe
